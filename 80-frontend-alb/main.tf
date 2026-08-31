f0053fa44c6237ace63b2f9ff9d6fe49bc2c8732
heman chandra <hemanchandra701@gmail.com>
2026-08-31 22:12:50 +0530

    jenkins

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -45,3 +45,43 @@ resource "aws_route53_record" "frontend_alb" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_lb_target_group" "frontend" {
+  name        = "${local.common_name}-frontend"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = local.vpc_id
+  target_type = "ip"
+  deregistration_delay = 60
+
+  health_check {
+    path = "/"
+    port     = "8080"
+    protocol = "HTTP"
+    interval = 30
+    healthy_threshold = 3
+    unhealthy_threshold = 3
+    matcher = "200-299"
+    timeout = 10
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_lb_listener_rule" "frontend" {
+  listener_arn = aws_lb_listener.frontend_alb.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+
+  condition {
+    host_header {
+      values = [
+        "${local.common_name}.${var.domain_name}"
+      ]
+    }
+  }
+}
